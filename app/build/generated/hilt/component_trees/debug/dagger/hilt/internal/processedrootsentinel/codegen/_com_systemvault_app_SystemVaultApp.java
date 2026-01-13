a2d971f3dc0ed888a16ced1350c56c051092b0aa
jerimie81 <jerimiepalmer81@gmail.com>
2026-01-13 00:39:42 -0500

    Remove generated build artifacts

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_systemvault_app_SystemVaultApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.systemvault.app.SystemVaultApp"
-)
-public final class _com_systemvault_app_SystemVaultApp {
-}
